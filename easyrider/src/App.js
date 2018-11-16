91b6202f7da8727f44b078ba23dacf377e483e0c
Alberto Huélamo <ahsalberto+dev@gmail.com>
2018-11-16 16:21:04 +0100

    antd + layout

--- easyrider/src/App.js
+++ easyrider/src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from "react";
 import { Provider, Subscribe } from "unstated";
+import { Layout } from "antd";
+
 // eslint-disable-next-line no-unused-vars
 import UNSTATED from "unstated-debug";
+
 import Map, { MapContainer } from "./components/Map";
 import "./App.css";
+
+const { Header, Footer, Sider, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <Provider>
-        <Subscribe to={[MapContainer]}>
-          {mapStore => (
-            <div className="App">
-              <Map id="map" mapStore={mapStore} />
-            </div>
-          )}
-        </Subscribe>
-      </Provider>
+      <div className="App">
+        <Provider>
+          <Subscribe to={[MapContainer]}>
+            {mapStore => (
+              <Layout className="full">
+                <Layout className="full">
+                  <Content className="full">
+                    <Map id="map" mapStore={mapStore} />
+                  </Content>
+                  <Footer>Timeline selector</Footer>
+                </Layout>
+                <Sider width="300" theme="light">
+                  Menu
+                </Sider>
+              </Layout>
+            )}
+          </Subscribe>
+        </Provider>
+      </div>
     );
   }
 }
