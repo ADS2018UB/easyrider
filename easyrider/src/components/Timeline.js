6c4ec16e725927e6e51b251e015519b60fcf0abf
aduenape7 <aduenape7@alumnes.ub.edu>
2018-11-22 02:52:37 +0100

    trying to show date in slider tooltip (unfinished)

--- easyrider/src/components/Timeline.js
+++ easyrider/src/components/Timeline.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Slider } from "antd";
+import moment from "moment";
 
 import { API_URL } from "../constants";
 
@@ -19,11 +20,19 @@ class Timeline extends Component {
     console.log("onAfterChange: ", value);
   }
 
+  formatter(value) {
+    const { day, hour } = this.state;
+    const diff = 5 * (value - 50);
+
+    return diff;
+  }
+
   render() {
     return (
       <div id="hour_slider">
         <Slider
           defaultValue={50}
+          tipFormatter={this.formatter.bind(this)}
           onChange={this.onChange.bind(this)}
           onAfterChange={this.onAfterChange.bind(this)}
         />
