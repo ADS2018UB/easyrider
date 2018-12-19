b4f9b98234a6e4db7d2f61c6d79294cdba4f80b9
Alberto Huélamo <ahsalberto+dev@gmail.com>
2018-12-19 16:55:26 +0100

    Added legend

--- easyrider/src/components/AppDescription.js
+++ easyrider/src/components/AppDescription.js
@@ -14,6 +14,20 @@ const AppDescription = () => (
       Use the search field if you want to look up a station by its name or by
       its ID.
     </p>
+    <div id="legend">
+      <p className="legend-item">
+        <img src="xs/marker_xs_0000.png" alt="empty" />
+        &nbsp;Empty Station
+      </p>
+      <p className="legend-item">
+        <img src="xs/marker_xs_0500.png" alt="some" />
+        &nbsp;Non empty station
+      </p>
+      <p className="legend-item">
+        <img src="xs/marker_xs_1000.png" alt="full" />
+        &nbsp;Full Station
+      </p>
+    </div>
   </div>
 );
 
